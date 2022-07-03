89cd873b5f3173f68c59e860bec819d9e931107b
MouktikaCherukupalli <102598571+MouktikaCherukupalli@users.noreply.github.com>
2022-07-03 15:03:17 +0530

    Add files via upload

---
+++ 1.1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Defining the function for generating uniform random numbers
+void uniform(char *str, int len)
+{
+int i;
+FILE *fp;
+
+fp = fopen(str,"w");
+//Generate numbers
+for (i = 0; i < len; i++)
+{
+fprintf(fp,"%lf\n",(double)rand()/RAND_MAX);
+}
+fclose(fp);
+
+}
+int  main(void) {
+//Uniform random numbers
+uniform("uni.dat", 1000000);
+return 0;
+}
